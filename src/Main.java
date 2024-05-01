4c27aade1b749bfd14e8a09fc05c4edfe046ebe5
Secretfi1e <beyzadeemin9@gmail.com>
2024-05-01 11:41:40 +0400

    string metodlari

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //Bir metod yaradın, bu metod vasitəsilə
+
+        //a hərflərini silsin. Çapda "bnn" çıxsın.
+
+        String text ="banana";
+        System.out.println(text.replace("a",""));
+    }
+}
